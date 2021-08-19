f3272bb1b3005183f5714c780ba23ff41e717384
Eduardo Alves Canuto <edutecni@gmail.com>
2021-08-19 16:09:59 -0300

    Ok

---
+++ edutecniDotnetCore2/Controllers/ProdutoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace edutecniDotnetCore2.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public string Index()
+        {
+            return "Esse é o método Action Padrão...";
+        }
+
+        public IActionResult Detalhe()
+        {
+            return View();
+        }
+    }
+}
